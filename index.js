582e5f29136eec020c298c97a4efb5bcf8ca1472
Samratcodebase <samratcodebase@gmail.com>
2026-02-09 23:58:07 +0530

    Refactor

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new Socket(httpServer);
 
+app.use(express.static("./public"))
 app.get("/me", (req, res) => {
   res.status(200).json({
     message: "Heiiiiiiiiii",
   });
 });
 
+io.on("Connection", (Socket) => {
+  console.log(Socket.id);
+});
 httpServer.listen(3000, () => {
   console.log("Server is running on 3000");
 });
