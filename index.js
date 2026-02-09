931b33616dcc301f8859338383319c7b1c90678b
Samratcodebase <samratcodebase@gmail.com>
2026-02-10 00:00:51 +0530

    Basic Socket IO

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from "express";
 import http from "http";
-import { Socket } from "socket.io";
+import { Server } from "socket.io";
 
 const app = express();
 const httpServer = http.createServer(app);
-const io = new Socket(httpServer);
+const io = new Server(httpServer);
 
-app.use(express.static("./public"))
+app.use(express.static("./public"));
 app.get("/me", (req, res) => {
   res.status(200).json({
     message: "Heiiiiiiiiii",
   });
 });
 
-io.on("Connection", (Socket) => {
-  console.log(Socket.id);
+io.on("connection", (Socket) => {
+  console.log("User ID:", Socket.id);
 });
 httpServer.listen(3000, () => {
   console.log("Server is running on 3000");
