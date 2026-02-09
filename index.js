1958057e094164cc5cd57c8fde46440447f0bb93
Samratcodebase <samratcodebase@gmail.com>
2026-02-10 01:24:50 +0530

    Basic Chat App

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
-app.use(express.static("./public"));
+app.use(express.static("public"));
 app.get("/me", (req, res) => {
   res.status(200).json({
     message: "Heiiiiiiiiii",
   });
 });
 
-io.on("connection", (Socket) => {
-  console.log("User ID:", Socket.id);
+io.on("connection", (socket) => {
+  // lowercase
+  console.log("User Connected", socket.id);
+
+  socket.on("client-message", (msg) => {
+    console.log("Received from client:", msg);
+    io.emit("server-message", msg);
+  });
 });
 httpServer.listen(3000, () => {
   console.log("Server is running on 3000");
